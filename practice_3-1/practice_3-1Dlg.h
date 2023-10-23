aacc0aad36e58058ce242134650b8c95a6e5e09c
Namju Kim <cmsong111@naver.com>
2023-10-23 17:10:05 +0900

    연습문제 3-1

---
+++ practice_3-1/practice_3-1Dlg.h
@@ -0,0 +1,46 @@
+﻿
+// practice_3-1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// Cpractice31Dlg 대화 상자
+class Cpractice31Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	Cpractice31Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PRACTICE_31_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int start_x;
+	int start_y;
+	int end_x;
+	int end_y;
+	int line_width;
+	int line_R;
+	int line_G;
+	int line_B;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+};
